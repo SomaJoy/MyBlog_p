d5386e7e0d909ba9d1a8d36a73b22268c1c072de
SomaJoy <somajoy1083.sj@gmail.com>
2024-01-05 16:59:53 +0530

    101: 05/01/24 Created myblog application - pushed data in database.

---
+++ src/main/java/com/myblog/service/impl/PostServiceImpl.java
@@ -0,0 +1,34 @@
+package com.myblog.service.impl;
+
+import com.myblog.entity.Post;
+import com.myblog.payload.PostDto;
+import com.myblog.repository.PostRepository;
+import com.myblog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto crteatePost(PostDto postDto) {
+
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savePost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+        dto.setTitle(savePost.getTitle());
+        dto.setDescription(savePost.getDescription());
+        dto.setContent(savePost.getContent());
+
+        return dto;
+    }
+}
